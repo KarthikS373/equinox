2eea79674c1fb26b6593f5ed8098bac79ec1c459
KarthikS373 <lit2021012@iiitl.ac.in>
2023-01-06 00:18:11 +0530

    Cute robo

--- src/js/experience/world/space/model.js
+++ src/js/experience/world/space/model.js
@@ -2,6 +2,7 @@ import * as THREE from "three"
 
 import Experience from "../.."
 import SpaceModel from "../../../../assets/models/space.glb"
+import RobotModel from "../../../../assets/models/robot.glb"
 
 class Model {
   constructor() {
@@ -11,11 +12,13 @@ class Model {
   }
 
   loadModel() {
-    this.components.loader.gltfLoader.load(SpaceModel, (gltf) => {
+    this.components.loader.gltfLoader.load(RobotModel, (gltf) => {
       this.gltf = gltf
 
       this.mesh = this.gltf.scene
-      this.mesh.scale.set(4, 4, 4)
+      this.mesh.scale.set(7, 7, 7)
+      this.mesh.rotation.y = Math.PI
+      this.mesh.position.y = -4
 
       this.scene.add(this.mesh)
 
@@ -27,27 +30,32 @@ class Model {
     this.mixer = new THREE.AnimationMixer(this.mesh)
     this.clips = this.gltf.animations
 
-    const action = this.mixer.clipAction(this.gltf.animations[0])
-    action.play()
+    this.action = this.mixer.clipAction(this.gltf.animations[0])
+    this.animate()
 
     this.initParallax()
+  }
+
+  animate() {
+    if (this.action) this.action.play()
   }
 
   update(delta) {
     if (this.mixer) this.mixer.update(delta * 0.001)
 
     if (this.mesh) {
-      this.mesh.rotation.y += delta * 0.0001
-      this.mesh.rotation.x += Math.sin(delta * 0.0005)
-
-      this.mesh.position.z = Math.sin(delta * 0.005) + Math.cos(delta * 0.0003)
+      // this.mesh.rotation.y += delta * 0.0001
+      // this.mesh.rotation.x += Math.sin(delta * 0.0005)
+      // this.mesh.position.z = Math.sin(delta * 0.005) + Math.cos(delta * 0.0003)
     }
   }
 
   initParallax() {
     window.addEventListener("mousemove", (e) => {
-      this.mesh.position.x = (e.clientX / this.sizes.width) * 1.5
-      this.mesh.position.y = (e.clientY / this.sizes.height) * 1.5
+      this.mesh.position.x += (e.clientX / this.sizes.width - 0.5) * 0.1
+      this.mesh.position.y += (e.clientY / this.sizes.height - 0.5) * 0.1
+
+      console.log(e.clientX / this.sizes.width)
     })
   }
 }
